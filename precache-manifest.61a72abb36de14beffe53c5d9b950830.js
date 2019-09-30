297c770a3ff88bea940c4b6a1b3dfcfb419a3c08
KalDhondup <kalsang.bios@outlook.com>
2019-09-30 21:40:49 +0530

    coping the build from create-react-app with --typescript and express

---
+++ precache-manifest.61a72abb36de14beffe53c5d9b950830.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03b65699a6c0c506bbd765a12fe44cb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac1af24dec729e112ea4",
+    "url": "/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "c2690f9eec16fbd3183f",
+    "url": "/static/js/2.aa06b81c.chunk.js"
+  },
+  {
+    "revision": "ac1af24dec729e112ea4",
+    "url": "/static/js/main.a0ca26e7.chunk.js"
+  },
+  {
+    "revision": "171ec9b8b00007179e9d",
+    "url": "/static/js/runtime-main.163d467e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
