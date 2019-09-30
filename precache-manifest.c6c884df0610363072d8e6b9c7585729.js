61173f0b24fbf30be251cdbca3188208de135242
KalDhondup <kalsang.bios@outlook.com>
2019-09-30 22:49:21 +0530

    Updates

---
+++ precache-manifest.c6c884df0610363072d8e6b9c7585729.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e90ef65a5a75807e5e8e5c685944829",
+    "url": "/kaldhondup/index.html"
+  },
+  {
+    "revision": "194e1294930da11913cc",
+    "url": "/kaldhondup/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "c2690f9eec16fbd3183f",
+    "url": "/kaldhondup/static/js/2.aa06b81c.chunk.js"
+  },
+  {
+    "revision": "194e1294930da11913cc",
+    "url": "/kaldhondup/static/js/main.ed9b324c.chunk.js"
+  },
+  {
+    "revision": "592bbcf2654070b9c210",
+    "url": "/kaldhondup/static/js/runtime-main.0ccd52a6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/kaldhondup/static/media/logo.25bf045c.svg"
+  }
+]);
